e5fd3d1f84fde54a6cdd71d876b3f177ba9fbdf2
babakoto <e.tokiniaina@gmail.com>
2021-06-06 02:16:44 +0200

    finish

--- lib/flutter_random_user.dart
+++ lib/flutter_random_user.dart
@@ -1,2 +1,4 @@
 library flutter_random_user;
-
+export 'package:flutter_random_user/random_data.dart';
+export 'package:flutter_random_user/json.dart';
+export 'package:flutter_random_user/random_user.dart';
