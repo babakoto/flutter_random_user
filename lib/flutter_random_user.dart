cf413dbf32960805b541c88c8c4ccc2fd5a0c2bc
babakoto <e.tokiniaina@gmail.com>
2021-06-07 14:24:26 +0200

    clear code

--- lib/flutter_random_user.dart
+++ lib/flutter_random_user.dart
@@ -1,9 +1,4 @@
 library flutter_random_user;
-
-import 'package:flutter_random_user/src/random_data.dart';
-import 'package:flutter_random_user/src/json.dart';
-import 'package:flutter_random_user/src/random_user.dart';
-
 export 'package:flutter_random_user/src/random_data.dart';
 export 'package:flutter_random_user/src/json.dart';
 export 'package:flutter_random_user/src/random_user.dart';
